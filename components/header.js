6710e21e8026712238cef1c0851ff41eb735a007
hlorellium <tdoregani@hotmail.com>
2021-01-06 15:35:01 +0300

    made name a link

--- components/header.js
+++ components/header.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import NavLink from './navLink';
 import styles from '../styles/header.module.css';
+import Link from 'next/link';
 
 const Header = () => {
     return (
         <div className={styles.wrapper}>
-            <h3 className={styles.name}>Pjotr Anohhin</h3>
+            <Link href="/">
+                <a className={styles.name}>Pjotr Anohhin</a>
+            </Link>
+
             <ul className={styles.links}>
                 <NavLink href="/projects">Projects</NavLink>
             </ul>
