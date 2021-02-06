3b4b6f56282fb6c98d38c9461b51aab620bc6b04
hlorella <tdoregani@hotmail.com>
2021-02-06 16:16:49 +0300

    added title

--- pages/_app.js
+++ pages/_app.js
@@ -14,6 +14,7 @@ function MyApp({ Component, pageProps }) {
                     content="initial-scale=1.0, width=device-width"
                 />
                 <link rel="shortcut icon" href="/favicon.ico" />
+                <title>Pjotr Anohhin</title>
             </Head>
             <ColorProvider>
                 <GlobalStyles />
--- pages/contact.js
+++ pages/contact.js
@@ -4,77 +4,84 @@ import { ReactComponent as EmailIcon } from '../public/icons/email.svg';
 import { ReactComponent as GithubIcon } from '../public/icons/github.svg';
 import { ReactComponent as LinkedinIcon } from '../public/icons/linkedin.svg';
 import { ReactComponent as TelegramIcon } from '../public/icons/telegram.svg';
+import Head from 'next/head';
 
 const Contact = () => {
     return (
-        <div
-            css={css`
-                width: 60vw;
-                .divider {
-                    width: 80px;
-                    height: 2px;
-                    background-color: var(--font);
-                    margin: 1rem 0;
-                }
-                span {
-                    opacity: 0.9;
-                }
-                a {
-                }
-                a:hover {
-                    text-decoration: underline;
-                }
-                svg {
-                    width: 1.5rem;
-                    height: 1.5rem;
-                    margin-right: 1rem;
-                }
-                svg path {
-                    fill: var(--font);
-                }
-                svg rect {
-                    fill: var(--font);
-                }
-                p {
-                    display: flex;
-                    align-items: center;
-                }
-            `}
-        >
-            <h2>Contact me</h2>
-            <div className="divider" />
-            <p>
-                <EmailIcon />
+        <>
+            <Head>
+                <title>Contact</title>
+                {/* <link rel="icon" href="/favicon.ico" /> */}
+            </Head>
+            <div
+                css={css`
+                    width: 60vw;
+                    .divider {
+                        width: 80px;
+                        height: 2px;
+                        background-color: var(--font);
+                        margin: 1rem 0;
+                    }
+                    span {
+                        opacity: 0.9;
+                    }
+                    a {
+                    }
+                    a:hover {
+                        text-decoration: underline;
+                    }
+                    svg {
+                        width: 1.5rem;
+                        height: 1.5rem;
+                        margin-right: 1rem;
+                    }
+                    svg path {
+                        fill: var(--font);
+                    }
+                    svg rect {
+                        fill: var(--font);
+                    }
+                    p {
+                        display: flex;
+                        align-items: center;
+                    }
+                `}
+            >
+                <h2>Contact me</h2>
+                <div className="divider" />
+                <p>
+                    <EmailIcon />
 
-                <a href="mailto: tdoregani@hotmail.com">
-                    tdoregani@hotmail.com
-                </a>
-            </p>
-            <p>
-                <LinkedinIcon />
+                    <a href="mailto: tdoregani@hotmail.com">
+                        tdoregani@hotmail.com
+                    </a>
+                </p>
+                <p>
+                    <LinkedinIcon />
 
-                <a
-                    target="_blank"
-                    href="https://www.linkedin.com/in/pjotr-anohhin-86475b1aa/"
-                >
-                    https://www.linkedin.com/in/pjotr-anohhin-86475b1aa/
-                </a>
-            </p>
-            <p>
-                <GithubIcon />
+                    <a
+                        target="_blank"
+                        href="https://www.linkedin.com/in/pjotr-anohhin-86475b1aa/"
+                    >
+                        https://www.linkedin.com/in/pjotr-anohhin-86475b1aa/
+                    </a>
+                </p>
+                <p>
+                    <GithubIcon />
 
-                <a target="_blank" href="https://github.com/hlorellium">
-                    https://github.com/hlorellium
-                </a>
-            </p>
-            <p>
-                <TelegramIcon />
+                    <a target="_blank" href="https://github.com/hlorellium">
+                        https://github.com/hlorellium
+                    </a>
+                </p>
+                <p>
+                    <TelegramIcon />
 
-                <a target="_blank" href="https://t.me/hlorellium">
-                    @hlorellium
-                </a>
-            </p>
-        </div>
+                    <a target="_blank" href="https://t.me/hlorellium">
+                        @hlorellium
+                    </a>
+                </p>
+            </div>
+        </>
     );
 };
 
